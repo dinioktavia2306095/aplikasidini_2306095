6430d3848375a5ad6a80b29118cbdd656c1fbc93
dinioktavia2306095 <2306095@itg.ac.id>
2026-03-09 19:36:40 +0700

    Update layout artikel

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,196 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String title = 'Aplikasi Dini Oktavia';
-    String name = 'Dini Oktavia';
-    String message = 'Halo nama saya $name, selamat datang di $title';
+    return const MaterialApp(home: MyHomePage());
+  }
+}
 
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(233, 8, 233, 1),
-          title: Text(title),
-        ),
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: const Color.fromARGB(255, 139, 9, 95),
-                width: 2,
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 254, 175, 244),
+        leading: const Icon(Icons.home),
+        title: const Text("Aplikasi Dini Oktavia"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                children: const [
+                  Icon(
+                    Icons.archive,
+                    size: 40.0,
+                    color: Color.fromARGB(255, 2, 61, 4),
+                  ),
+                  Text(
+                    "Artikel Baru",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
               ),
-            ),
-            child: Text(message),
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                  child: Column(
+                    children: [
+                      Image.network(
+                        'https://picsum.photos/200/300?grayscale',
+                        height: 150,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(height: 15),
+                      const Text(
+                        "Artikel 1",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 249, 23, 143),
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          "Hai! Nama saya dini oktavia saya sedang belajar flutter",
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(fontSize: 14, color: Colors.black87),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: const [
+                  Text(
+                    'Komentar',
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  SizedBox(height: 10),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 3),
+                              Text(
+                                "Hobi",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontStyle: FontStyle.normal,
+                                  letterSpacing: 1.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Membaca buku, Menulis, Berenang, Bermain musik, Fotografi, Memasak, Berkebun, Bersepeda, Hiking, dan Traveling.",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 3),
+                              Text(
+                                "Kesukaan",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontStyle: FontStyle.normal,
+                                  letterSpacing: 1.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Mie Ayam, Bakso, Nasi Goreng, Sate, Martabak, dan semua makanan yang enak.",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(width: 3),
+                              Text(
+                                "Ketidaksukaan",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontStyle: FontStyle.normal,
+                                  letterSpacing: 1.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Kecoa, laba-laba, tikus, ular, dan semua hewan yang menjijikkan.",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
